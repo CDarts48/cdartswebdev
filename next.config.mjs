219c0ec51b3ad93d45fe84c882f458ccc884d844
Corey Donahue <cdartswebdev@gmail.com>
2025-02-20 12:37:06 -0700

    little more exporting

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'shot.screenshotapi.net',
-          pathname: '/v3/screenshot',
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
+  output: "export", // Enables static export
+  trailingSlash: true, // Helps with correct routing on GitHub Pages
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'shot.screenshotapi.net',
+        pathname: '/v3/screenshot',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
