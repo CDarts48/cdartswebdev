d428b1e09016d68df3f2a1ffc9381ef6c810aa7c
Corey Donahue <cdartswebdev@gmail.com>
2025-02-25 09:41:02 -0700

    got tab looking better

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   output: "export", // Enables static export
   trailingSlash: true, // Helps with correct routing on GitHub Pages
   images: {
+    unoptimized: true, // Disable Image Optimization API
     remotePatterns: [
       {
         protocol: 'https',
