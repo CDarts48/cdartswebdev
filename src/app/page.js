df58d18dffb4c2e0773774672c0e70920f5eeb72
Corey Donahue <cdartswebdev@gmail.com>
2025-06-05 13:30:21 -0600

    more styling

--- src/app/page.js
+++ src/app/page.js
@@ -198,7 +198,7 @@ export default function Home() {
         </Link>
         <Link
           href="#projects"
-          className="inline-flex h-9 items-center justify-center rounded-md border border-stone-200 bg-stone-50 px-4 py-2 text-sm font-medium shadow-sm transition-colors hover:bg-stone-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300 drop-shadow"
+          className="inline-flex h-9 items-center justify-center rounded-md border border-stone-200 bg-stone-50 px-4 py-2 text-sm font-medium shadow-sm transition-colors hover:bg-stone-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300 drop-shadow text-black dark:text-white"
         >
           View Projects
         </Link>
